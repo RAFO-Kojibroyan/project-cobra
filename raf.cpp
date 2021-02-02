afaee74776a6ff341d02620319069612877cb4c2
RAFO-Kojibroyan <78412568+RAFO-Kojibroyan@users.noreply.github.com>
2021-02-02 18:04:29 +0400

    Add files via upload

---
+++ raf.cpp
@@ -0,0 +1,51 @@
+//glux 1 xndir 1 ktpi 2 , 48, 14;
+//glux 1 xndir 2 da da da
+//glux 1 xndir 3 incude dem@ # chka
+//glux 1 xndir 4 # dnum es tpuma Hello Buggy World
+//glux 2 xndir 1 chi tpum vorovhetev graca std::Cout std::cout i tex@ 
+//glux 2 xndir 2 mi tox verev grel em 
+//glux 2 xndir 3 
+/* #include <iostream>
+using namespace std;
+int DemoConsoleOutput ();
+int main()
+{
+int DemoConsoleOutput ();
+return 0;
+}
+int DemoConsoleOutput ()
+{
+  cout << "Простой строковый литерал" << endl;
+  cout << "Запись числа пять: " << 5 << endl;
+  cout << "Выполнение деления 10/5 = " << 10/5 << endl;
+  cout << "Пи примерно равно 22/7 = " << 22/7 << endl;
+  cout << "Более точно Пи равно 22/7 = " << 22.0/7 << endl;
+  return 0;
+}
+
+#include <iostream>
+using namespace std;
+int DemoConsoleOutput ();
+int main ()
+{
+  int DemoConsoleOutput ();
+  return 0;
+}
+int DemoConsoleOutput ()
+{
+  cout<<"Простой строковый литерал" << endl;
+  cout << "Запись числа пять: " << 5 << endl;
+  cout << "Выполнение деления 10-5 = " << 10-5 << endl;
+  cout << "Пи примерно равно 22-7 = " << 22-7 << endl;
+  cout << "Более точно Пи равно 22-7 = " << 22.0 - 7 << endl;
+  return 0;
+}
+
+
+
+
+
+
+
+
+
